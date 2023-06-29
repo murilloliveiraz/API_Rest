ade2fafa3a419c9fad9055edd125eb0f7804db9e
Murillo Oliveira <murillo1047@gmail.com>
2023-06-28 21:04:12 -0300

    feat : sequelize configs

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Murillo',
+      sobrenome : 'Oliveira',
+      email: 'murillo@gmail.com',
+      idade: 17,
+      peso: 62,
+      altura: 1.8,
     });
+    res.json(novoAluno);
   }
 }
 
